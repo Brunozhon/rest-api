43256ad0efbc7b161ea668b26ec57c8b25b2513b
Bruno Zhong <69879040+Brunozhon@users.noreply.github.com>
2020-10-03 17:58:31 -0400

    Update url-query.js

--- server/url-query.js
+++ server/url-query.js
@@ -3,8 +3,24 @@
   var url = require("url");
   var adr = url.parse(window.location.href);
   var q = adr.query;
-  if (q.json === undefined) {console.error("q.json must not be undefined"); return;}
-  if ("length" in q.json != false) {return;}
+  if (q.json === undefined || q.json === '') {
+    console.error("q.json must not be undefined");
+    http.createServer(function(req, res) {
+      res.writeHead(403, {"Content-Type":"application/json"});
+      res.write('{"meta":{"status":403, "statusText":"Forbidden"}, "data":null}');
+      res.end()
+    }).listen(443);
+    return;
+  }
+  if ("length" in q.json != false) {
+    console.error("q.json is not a object");
+    http.createServer(function(req, res) {
+      res.writeHead(403, {"Content-Type":"application/json"});
+      res.write('{"meta":{"status":403, "statusText":"Forbidden"}, "data":null}');
+      res.end()
+    });
+    return;
+  }
   http.createServer(function(req, res) {
     res.writeHead(200, {"Content-Type":"application/json"});
     res.write('{"meta":{"status":200, "statusText":"OK"}, "data":' + q.json + "}");
